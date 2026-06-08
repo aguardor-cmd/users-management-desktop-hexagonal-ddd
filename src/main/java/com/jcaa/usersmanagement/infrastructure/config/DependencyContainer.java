771814823c16f91f6918199939cf680fece8de73
Eduardo <aguardo@unicartagena.edu.co>
2026-06-08 12:34:09 -0500

    fix: corrected constructor arguments for UpdateCandidatoService and DeleteCandidatoService

--- src/main/java/com/jcaa/usersmanagement/infrastructure/config/DependencyContainer.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/config/DependencyContainer.java
@@ -91,9 +91,9 @@ public final class DependencyContainer {
     final CreateCandidatoUseCase createCandidatoUseCase =
         new CreateCandidatoService(candidatoRepository, candidatoRepository, validator);
     final UpdateCandidatoUseCase updateCandidatoUseCase =
-        new UpdateCandidatoService(candidatoRepository, candidatoRepository, validator);
+        new UpdateCandidatoService(candidatoRepository, candidatoRepository, candidatoRepository, validator);
     final DeleteCandidatoUseCase deleteCandidatoUseCase =
-        new DeleteCandidatoService(candidatoRepository, validator);
+        new DeleteCandidatoService(candidatoRepository, candidatoRepository, validator);
     final GetCandidatoByIdUseCase getCandidatoByIdUseCase = new GetCandidatoByIdService(candidatoRepository, validator);
     final GetAllCandidatosUseCase getAllCandidatosUseCase = new GetAllCandidatosService(candidatoRepository);
 
